405db3129de1920c74d229510a673541cb684c8e
Spongee <vezcua@gmail.com>
2024-04-06 14:55:51 +0200

    Initial Commit

---
+++ RickANDMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickANDMorty
+//
+//  Created by Carlo Fontolan on 06/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
